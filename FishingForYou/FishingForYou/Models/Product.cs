49c2b416433dc85b153716bf90679ed8c6272935
elizioni <eli.zioni@gmail.com>
2019-01-01 22:32:10 +0200

    nodels :Ad,Ordreritem and product

---
+++ FishingForYou/FishingForYou/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishingForYou.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Publisher { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string FirstImage { get; set; }
+        public string SecondImage { get; set; }
+        public int OrderId { get; set; }
+        public int AdId { get; set; }
+    }
+}
