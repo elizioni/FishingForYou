bf54f0c492d7bd6ad27614a02d9a5540c2170dca
elizioni <eli.zioni@gmail.com>
2019-01-04 08:39:53 +0200

    models : order.cs by Yair

---
+++ FishingForYou/FishingForYou/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FishingForYou.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public string Email { get; set; }
+
+        public double SumAmount { get; set; }
+        public int Quantity
+        {
+            get
+            {
+                int sumQuantity = 0;
+
+                foreach (var item in OrderItems)
+                {
+                    sumQuantity += item.Quantity;
+                }
+                return sumQuantity;
+
+            }
+        }
+    }
+}
